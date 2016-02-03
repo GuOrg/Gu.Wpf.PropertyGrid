11fb0561b888471921514f660dcede02d6e09be1
Johan Larsson <johan_gavle1@hotmail.com>
2016-02-03 12:21:58 +0100

    amend

--- Gu.Wpf.PropertyGrid.Controls/OneWayToSourceBinding/OneWayToSource.cs
+++ Gu.Wpf.PropertyGrid.Controls/OneWayToSourceBinding/OneWayToSource.cs
@@ -3,7 +3,6 @@
     using System;
     using System.Windows;
     using System.Windows.Data;
-    using System.Windows.Markup;
 
     public static class OneWayToSource
     {
@@ -74,21 +73,6 @@
             {
                 d.SetCurrentValue(TargetProxyProperty, e.NewValue);
             }
-        }
-    }
-
-    [MarkupExtensionReturnType(typeof(OneWayToSource.ProxyBinding))]
-    public class Paths : MarkupExtension
-    {
-        public DependencyProperty From { get; set; }
-
-        public string To { get; set; }
-
-        public override object ProvideValue(IServiceProvider serviceProvider)
-        {
-            var provideValueTarget = (IProvideValueTarget)serviceProvider.GetService(typeof(IProvideValueTarget));
-            var targetObject = (UIElement)provideValueTarget.TargetObject;
-            return new OneWayToSource.ProxyBinding(targetObject, this.From, this.To);
         }
     }
 }
