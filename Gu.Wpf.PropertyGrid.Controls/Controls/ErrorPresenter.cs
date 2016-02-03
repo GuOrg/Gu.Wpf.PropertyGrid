2150f3bedcc0d6984dbf62e900fc484ba449ccdd
Johan Larsson <johan_gavle1@hotmail.com>
2016-02-03 16:01:15 +0100

    InfoPresenter WIP

---
+++ Gu.Wpf.PropertyGrid.Controls/Controls/ErrorPresenter.cs
@@ -0,0 +1,13 @@
+﻿namespace Gu.Wpf.PropertyGrid
+{
+    using System.Windows;
+    using System.Windows.Controls;
+
+    public class ErrorPresenter : ContentPresenter
+    {
+        static ErrorPresenter()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ErrorPresenter), new FrameworkPropertyMetadata(typeof(ErrorPresenter)));
+        }
+    }
+}
---
+++ Gu.Wpf.PropertyGrid.Controls/Controls/InfoPresenter.cs
@@ -0,0 +1,13 @@
+﻿namespace Gu.Wpf.PropertyGrid
+{
+    using System.Windows;
+    using System.Windows.Controls;
+
+    public class InfoPresenter : ContentPresenter
+    {
+        static InfoPresenter()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(InfoPresenter), new FrameworkPropertyMetadata(typeof(InfoPresenter)));
+        }
+    }
+}
