ef675a6bbc03b5f0ad693d9d84dc3110efac8540
Johan Larsson <johan_gavle1@hotmail.com>
2016-04-18 21:10:05 +0200

    Update references. Paket auto-restore on
    - New packages 2.1.0.0

--- Gu.Wpf.PropertyGrid.Controls/Properties/AssemblyInfo.cs
+++ Gu.Wpf.PropertyGrid.Controls/Properties/AssemblyInfo.cs
@@ -50,8 +50,8 @@ using System.Windows.Markup;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.1.0")]
-[assembly: AssemblyFileVersion("2.0.1.0")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
 
 [assembly: InternalsVisibleTo("Gu.Wpf.PropertyGrid.Tests", AllInternalsVisible = true)]
 [assembly: InternalsVisibleTo("Gu.Wpf.PropertyGrid.NumericSettingControls", AllInternalsVisible = true)]
