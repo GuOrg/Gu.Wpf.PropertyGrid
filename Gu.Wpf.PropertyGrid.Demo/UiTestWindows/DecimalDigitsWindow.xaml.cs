80744c1cbd263c01babeb9b83f9c18781d65a7dc
Johan Larsson <johan_gavle1@hotmail.com>
2016-06-28 01:10:37 +0200

    More test windows, time for some tests now.

---
+++ Gu.Wpf.PropertyGrid.Demo/UiTestWindows/DecimalDigitsWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Gu.Wpf.PropertyGrid.Demo.UiTestWindows
+{
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for DecimalDigitsWindow.xaml
+    /// </summary>
+    public partial class DecimalDigitsWindow : Window
+    {
+        public DecimalDigitsWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Gu.Wpf.PropertyGrid.Demo/UiTestWindows/UnitsWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Gu.Wpf.PropertyGrid.Demo.UiTestWindows
+{
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for UnitsWindow.xaml
+    /// </summary>
+    public partial class UnitsWindow : Window
+    {
+        public UnitsWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
