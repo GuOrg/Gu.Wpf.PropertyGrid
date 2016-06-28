84a7627a9788d01d7ef2cb6afe75b258be8b7c63
Johan Larsson <johan_gavle1@hotmail.com>
2016-06-28 22:53:21 +0200

    SelectorSettingControlTests

---
+++ Gu.Wpf.PropertyGrid.Demo/UiTestWindows/SelectorSettingControlWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Gu.Wpf.PropertyGrid.Demo.UiTestWindows
+{
+    using System.Windows;
+    /// <summary>
+    /// Interaction logic for SelectorSettingControlWindow.xaml
+    /// </summary>
+    public partial class SelectorSettingControlWindow : Window
+    {
+        public SelectorSettingControlWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
