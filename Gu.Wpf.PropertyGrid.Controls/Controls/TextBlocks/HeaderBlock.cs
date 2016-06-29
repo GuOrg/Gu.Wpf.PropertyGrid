87145cb9207ec4e8998e7c6858f4c0924da6b82d
Johan Larsson <johan_gavle1@hotmail.com>
2016-06-29 19:19:42 +0200

    Cleanup ColumnsWindow, write some tests now.

--- Gu.Wpf.PropertyGrid.Controls/Controls/TextBlocks/HeaderBlock.cs
+++ Gu.Wpf.PropertyGrid.Controls/Controls/TextBlocks/HeaderBlock.cs
@@ -4,14 +4,10 @@ namespace Gu.Wpf.PropertyGrid
     using System.Windows.Automation.Peers;
     using System.Windows.Controls;
 
-    using JetBrains.Annotations;
-
     public class HeaderBlock : TextBlock
     {
         static HeaderBlock()
         {
-            DefaultStyleKeyProperty.OverrideMetadata(typeof(HeaderBlock), new FrameworkPropertyMetadata(typeof(HeaderBlock)));
-            MarginProperty.OverrideMetadataWithDefaultValue(typeof(HeaderBlock), new Thickness(0, 0, 2, 0));
             VerticalAlignmentProperty.OverrideMetadataWithDefaultValue(typeof(HeaderBlock), VerticalAlignment.Center);
             HorizontalAlignmentProperty.OverrideMetadataWithDefaultValue(typeof(HeaderBlock), HorizontalAlignment.Left);
         }
