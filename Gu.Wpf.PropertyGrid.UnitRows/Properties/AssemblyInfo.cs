3cd20d158b6fb84c06e76fc4f9b1db25416b3ace
Johan Larsson <johan_gavle1@hotmail.com>
2016-07-02 23:02:47 +0200

    Update XmlnsDefinition for unit rows.

--- Gu.Wpf.PropertyGrid.UnitRows/Properties/AssemblyInfo.cs
+++ Gu.Wpf.PropertyGrid.UnitRows/Properties/AssemblyInfo.cs
@@ -42,5 +42,5 @@ using System.Windows.Markup;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.0.0.0")]
 [assembly: AssemblyFileVersion("3.0.0.0")]
-[assembly: XmlnsDefinition("http://gu.se/PropertyGrid", "Gu.Wpf.PropertyGrid")]
+[assembly: XmlnsDefinition("http://gu.se/PropertyGrid", "Gu.Wpf.PropertyGrid.UnitRows")]
 [assembly: XmlnsPrefix("http://gu.se/PropertyGrid", "propertyGrid")]
