5180df9233e308eac4bbfa1175de1edf1bdc2d0b
Johan Larsson <johan_gavle1@hotmail.com>
2016-07-03 12:50:51 +0200

    wip

--- Gu.Wpf.PropertyGrid/Controls/RowInfoPresenter.cs
+++ Gu.Wpf.PropertyGrid/Controls/RowInfoPresenter.cs
@@ -7,35 +7,10 @@
 
     public class RowInfoPresenter : Control
     {
-        public static readonly DependencyProperty TargetProperty = DependencyProperty.Register(
-            "Target",
-            typeof(Row),
-            typeof(RowInfoPresenter),
-            new PropertyMetadata(default(Row)));
-
         static RowInfoPresenter()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(RowInfoPresenter), new FrameworkPropertyMetadata(typeof(RowInfoPresenter)));
             FocusableProperty.OverrideMetadataWithDefaultValue(typeof(RowInfoPresenter), false);
-        }
-
-        public Row Target
-        {
-            get { return (Row)this.GetValue(TargetProperty); }
-            set { this.SetValue(TargetProperty, value); }
-        }
-
-        protected override void OnInitialized(EventArgs e)
-        {
-            if (this.Target == null)
-            {
-                var target = this.Ancestors()
-                                 .OfType<Row>()
-                                 .FirstOrDefault();
-                this.SetCurrentValue(TargetProperty, target);
-            }
-
-            base.OnInitialized(e);
         }
     }
 }
