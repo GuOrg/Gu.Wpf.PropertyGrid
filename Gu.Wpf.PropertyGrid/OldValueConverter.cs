f1d1ab7c15cb681fbda0cc11b8a9a41d9fd4c0d4
Johan Larsson <johan_gavle1@hotmail.com>
2016-07-06 17:48:08 +0200

    Bind old value with converter for remaining.

---
+++ Gu.Wpf.PropertyGrid/OldValueConverter.cs
@@ -0,0 +1,21 @@
+﻿namespace Gu.Wpf.PropertyGrid
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    public class OldValueConverter : IMultiValueConverter
+    {
+        public static readonly OldValueConverter Default = new OldValueConverter();
+
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return $"Old value: {values[1]}";
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
