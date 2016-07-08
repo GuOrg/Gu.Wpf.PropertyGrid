1461cc79c6eb9d1b0757c26686fb45dd6a4409cb
Johan Larsson <johan_gavle1@hotmail.com>
2016-07-08 13:34:18 +0200

    Fix plain view.

---
+++ Gu.Wpf.PropertyGrid.Demo/UiTestWindows/LengthRowWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Gu.Wpf.PropertyGrid.Demo.UiTestWindows
+{
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for LengthRowWindow.xaml
+    /// </summary>
+    public partial class LengthRowWindow : Window
+    {
+        public LengthRowWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Gu.Wpf.PropertyGrid.Demo/UiTestWindows/StyledOldValue.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Gu.Wpf.PropertyGrid.Demo.UiTestWindows
+{
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for StyledOldValue.xaml
+    /// </summary>
+    public partial class StyledOldValue : Window
+    {
+        public StyledOldValue()
+        {
+            InitializeComponent();
+        }
+    }
+}
