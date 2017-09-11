f09141a46259170593bb44bbaff4fc2ab1f0378d
Johan Larsson <johan_gavle1@hotmail.com>
2017-09-11 03:21:32 +0200

    Refactor OldValueTests

--- Gu.Wpf.PropertyGrid.UiTests/OldValueTests.cs
+++ Gu.Wpf.PropertyGrid.UiTests/OldValueTests.cs
@@ -1,164 +1,182 @@
 ﻿namespace Gu.Wpf.PropertyGrid.UiTests
 {
+    using Gu.Wpf.UiAutomation;
     using NUnit.Framework;
 
-    using TestStack.White.UIItems;
-
-    public class OldValueTests : WindowTests
+    public class OldValueTests
     {
-        private Button loseFocusButton;
-        private Button saveButton;
+        private static readonly string WindowName = "OldValueWindow";
 
-        protected override string WindowName { get; } = "OldValueWindow";
-
-        [OneTimeSetUp]
-        public override void OneTimeSetUp()
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
         {
-            base.OneTimeSetUp();
-            this.loseFocusButton = this.Window.GetByText<Button>("lose focus");
-            this.saveButton = this.Window.GetByText<Button>("save");
+            Application.KillLaunched(Info.ExeFileName);
         }
 
         [SetUp]
         public void SetUp()
         {
-            var groupBox = this.Window.GetByText<GroupBox>("default");
-            groupBox.FindRow("string").Value<TextBox>().Text = "abc";
-            groupBox.FindRow("double").Value<TextBox>().Text = "1.2";
-            groupBox.FindRow("length").Value<TextBox>().Text = "2.3";
-            this.saveButton.Click();
+            using (var app = Application.AttachOrLaunch(Info.ExeFileName, WindowName))
+            {
+                var window = app.MainWindow;
+                var groupBox = window.FindGroupBox("default");
+                groupBox.FindTextBoxRow("string").Value().Text = "abc";
+                groupBox.FindTextBoxRow("double").Value().Text = "1.2";
+                groupBox.FindTextBoxRow("length").Value().Text = "2.3";
+                window.FindButton("save").Click();
+            }
         }
 
         [Test]
         public void Default()
         {
-            var groupBox = this.Window.GetByText<GroupBox>("default");
+            using (var app = Application.AttachOrLaunch(Info.ExeFileName, WindowName))
+            {
+                var window = app.MainWindow;
+                var groupBox = window.FindGroupBox("default");
 
-            var stringRow = groupBox.FindRow("string");
-            stringRow.Value<TextBox>().Text = "g";
-            var oldStringValue = stringRow.Info();
-            Assert.AreEqual("Old value: abc", oldStringValue.Text);
+                var stringRow = groupBox.FindTextBoxRow("string");
+                stringRow.Value().Text = "g";
+                var oldStringValue = stringRow.Info();
+                Assert.AreEqual("Old value: abc", oldStringValue.Text);
 
-            var doubleRow = groupBox.FindRow("double");
-            doubleRow.Value<TextBox>().Text = "1.23";
-            var oldDoubleValue = doubleRow.Info();
-            Assert.AreEqual("Old value: 1.2", oldDoubleValue.Text);
+                var doubleRow = groupBox.FindTextBoxRow("double");
+                doubleRow.Value().Text = "1.23";
+                var oldDoubleValue = doubleRow.Info();
+                Assert.AreEqual("Old value: 1.2", oldDoubleValue.Text);
 
-            var lengthRow = groupBox.FindRow("length");
-            lengthRow.Value<TextBox>().Text = "2";
-            var oldLengthValue = lengthRow.Info();
-            Assert.AreEqual("Old value: 2.3\u00A0mm", oldLengthValue.Text);
+                var lengthRow = groupBox.FindTextBoxRow("length");
+                lengthRow.Value().Text = "2";
+                var oldLengthValue = lengthRow.Info();
+                Assert.AreEqual("Old value: 2.3\u00A0mm", oldLengthValue.Text);
 
-            this.saveButton.Click();
-            Assert.AreEqual("", oldStringValue.Text);
-            Assert.AreEqual("", oldDoubleValue.Text);
-            Assert.AreEqual("", oldLengthValue.Text);
+                window.FindButton("save").Click();
+                Assert.AreEqual("", oldStringValue.Text);
+                Assert.AreEqual("", oldDoubleValue.Text);
+                Assert.AreEqual("", oldLengthValue.Text);
+            }
         }
 
         [Test]
         public void Inherits()
         {
-            var groupBox = this.Window.GetByText<GroupBox>("inherits");
+            using (var app = Application.AttachOrLaunch(Info.ExeFileName, WindowName))
+            {
+                var window = app.MainWindow;
+                var groupBox = window.FindGroupBox("inherits");
 
-            var stringRow = groupBox.FindRow("string");
-            stringRow.Value<TextBox>().Text = "g";
-            var oldStringValue = stringRow.Info();
-            Assert.AreEqual("before: abc", oldStringValue.Text);
+                var stringRow = groupBox.FindTextBoxRow("string");
+                stringRow.Value().Text = "g";
+                var oldStringValue = stringRow.Info();
+                Assert.AreEqual("before: abc", oldStringValue.Text);
 
-            var doubleRow = groupBox.FindRow("double");
-            doubleRow.Value<TextBox>().Text = "1.23";
-            var oldDoubleValue = doubleRow.Info();
-            Assert.AreEqual("before: 1.2", oldDoubleValue.Text);
+                var doubleRow = groupBox.FindTextBoxRow("double");
+                doubleRow.Value().Text = "1.23";
+                var oldDoubleValue = doubleRow.Info();
+                Assert.AreEqual("before: 1.2", oldDoubleValue.Text);
 
-            var lengthRow = groupBox.FindRow("length");
-            lengthRow.Value<TextBox>().Text = "2";
-            var oldLengthValue = lengthRow.Info();
-            Assert.AreEqual("before: 2.3\u00A0mm", oldLengthValue.Text);
+                var lengthRow = groupBox.FindTextBoxRow("length");
+                lengthRow.Value().Text = "2";
+                var oldLengthValue = lengthRow.Info();
+                Assert.AreEqual("before: 2.3\u00A0mm", oldLengthValue.Text);
 
-            this.saveButton.Click();
-            Assert.AreEqual("", oldStringValue.Text);
-            Assert.AreEqual("", oldDoubleValue.Text);
-            Assert.AreEqual("", oldLengthValue.Text);
+                window.FindButton("save").Click();
+                Assert.AreEqual("", oldStringValue.Text);
+                Assert.AreEqual("", oldDoubleValue.Text);
+                Assert.AreEqual("", oldLengthValue.Text);
+            }
         }
 
         [Test]
         public void Explicit()
         {
-            var groupBox = this.Window.GetByText<GroupBox>("explicit");
+            using (var app = Application.AttachOrLaunch(Info.ExeFileName, WindowName))
+            {
+                var window = app.MainWindow;
+                var groupBox = window.FindGroupBox("explicit");
 
-            var stringRow = groupBox.FindRow("string");
-            stringRow.Value<TextBox>().Text = "g";
-            var oldStringValue = stringRow.Info();
-            Assert.AreEqual("before: abc", oldStringValue.Text);
+                var stringRow = groupBox.FindTextBoxRow("string");
+                stringRow.Value().Text = "g";
+                var oldStringValue = stringRow.Info();
+                Assert.AreEqual("before: abc", oldStringValue.Text);
 
-            var doubleRow = groupBox.FindRow("double");
-            doubleRow.Value<TextBox>().Text = "1.23";
-            var oldDoubleValue = doubleRow.Info();
-            Assert.AreEqual("before: 1.2", oldDoubleValue.Text);
+                var doubleRow = groupBox.FindTextBoxRow("double");
+                doubleRow.Value().Text = "1.23";
+                var oldDoubleValue = doubleRow.Info();
+                Assert.AreEqual("before: 1.2", oldDoubleValue.Text);
 
-            var lengthRow = groupBox.FindRow("length");
-            lengthRow.Value<TextBox>().Text = "2";
-            var oldLengthValue = lengthRow.Info();
-            Assert.AreEqual("before: 2.3\u00A0mm", oldLengthValue.Text);
+                var lengthRow = groupBox.FindTextBoxRow("length");
+                lengthRow.Value().Text = "2";
+                var oldLengthValue = lengthRow.Info();
+                Assert.AreEqual("before: 2.3\u00A0mm", oldLengthValue.Text);
 
-            this.saveButton.Click();
-            Assert.AreEqual("", oldStringValue.Text);
-            Assert.AreEqual("", oldDoubleValue.Text);
-            Assert.AreEqual("", oldLengthValue.Text);
+                window.FindButton("save").Click();
+                Assert.AreEqual("", oldStringValue.Text);
+                Assert.AreEqual("", oldDoubleValue.Text);
+                Assert.AreEqual("", oldLengthValue.Text);
+            }
         }
 
         [Test]
         public void Bound()
         {
-            var groupBox = this.Window.GetByText<GroupBox>("bound");
+            using (var app = Application.AttachOrLaunch(Info.ExeFileName, WindowName))
+            {
+                var window = app.MainWindow;
+                var groupBox = window.FindGroupBox("bound");
 
-            var stringRow = groupBox.FindRow("string");
-            stringRow.Value<TextBox>().Text = "g";
-            var oldStringValue = stringRow.Info();
-            Assert.AreEqual("changed", oldStringValue.Text);
+                var stringRow = groupBox.FindTextBoxRow("string");
+                stringRow.Value().Text = "g";
+                var oldStringValue = stringRow.Info();
+                Assert.AreEqual("changed", oldStringValue.Text);
 
-            var doubleRow = groupBox.FindRow("double");
-            doubleRow.Value<TextBox>().Text = "1.23";
-            var oldDoubleValue = doubleRow.Info();
-            Assert.AreEqual("changed", oldDoubleValue.Text);
+                var doubleRow = groupBox.FindTextBoxRow("double");
+                doubleRow.Value().Text = "1.23";
+                var oldDoubleValue = doubleRow.Info();
+                Assert.AreEqual("changed", oldDoubleValue.Text);
 
-            var lengthRow = groupBox.FindRow("length");
-            lengthRow.Value<TextBox>().Text = "2";
-            var oldLengthValue = lengthRow.Info();
-            Assert.AreEqual("changed", oldLengthValue.Text);
+                var lengthRow = groupBox.FindTextBoxRow("length");
+                lengthRow.Value().Text = "2";
+                var oldLengthValue = lengthRow.Info();
+                Assert.AreEqual("changed", oldLengthValue.Text);
 
-            groupBox.FindRow("old value format").Value<TextBox>().Text = "before: {0}";
-            this.loseFocusButton.Click();
+                groupBox.FindTextBoxRow("old value format").Value().Text = "before: {0}";
+                window.FindButton("lose focus").Click();
 
-            Assert.AreEqual("before: abc", oldStringValue.Text);
-            Assert.AreEqual("before: 1.2", oldDoubleValue.Text);
-            Assert.AreEqual("before: 2.3\u00A0mm", oldLengthValue.Text);
+                Assert.AreEqual("before: abc", oldStringValue.Text);
+                Assert.AreEqual("before: 1.2", oldDoubleValue.Text);
+                Assert.AreEqual("before: 2.3\u00A0mm", oldLengthValue.Text);
 
-            this.saveButton.Click();
-            Assert.AreEqual("", oldStringValue.Text);
-            Assert.AreEqual("", oldDoubleValue.Text);
-            Assert.AreEqual("", oldLengthValue.Text);
+                window.FindButton("save").Click();
+                Assert.AreEqual("", oldStringValue.Text);
+                Assert.AreEqual("", oldDoubleValue.Text);
+                Assert.AreEqual("", oldLengthValue.Text);
+            }
         }
 
         [Test]
         public void NoOldValue()
         {
-            var groupBox = this.Window.GetByText<GroupBox>("no old datacontext");
+            using (var app = Application.AttachOrLaunch(Info.ExeFileName, WindowName))
+            {
+                var window = app.MainWindow;
+                var groupBox = window.FindGroupBox("no old datacontext");
 
-            var stringRow = groupBox.FindRow("string");
-            stringRow.Value<TextBox>().Text = "g";
-            Assert.AreEqual("", stringRow.Info().Text);
+                var stringRow = groupBox.FindTextBoxRow("string");
+                stringRow.Value().Text = "g";
+                Assert.AreEqual("", stringRow.Info().Text);
 
-            var doubleRow = groupBox.FindRow("double");
-            doubleRow.Value<TextBox>().Text = "1.23";
-            Assert.AreEqual("", stringRow.Info().Text);
-            Assert.AreEqual("", doubleRow.Info().Text);
+                var doubleRow = groupBox.FindTextBoxRow("double");
+                doubleRow.Value().Text = "1.23";
+                Assert.AreEqual("", stringRow.Info().Text);
+                Assert.AreEqual("", doubleRow.Info().Text);
 
-            var lengthRow = groupBox.FindRow("length");
-            lengthRow.Value<TextBox>().Text = "2";
-            Assert.AreEqual("", stringRow.Info().Text);
-            Assert.AreEqual("", doubleRow.Info().Text);
-            Assert.AreEqual("", lengthRow.Info().Text);
+                var lengthRow = groupBox.FindTextBoxRow("length");
+                lengthRow.Value().Text = "2";
+                Assert.AreEqual("", stringRow.Info().Text);
+                Assert.AreEqual("", doubleRow.Info().Text);
+                Assert.AreEqual("", lengthRow.Info().Text);
+            }
         }
     }
 }
