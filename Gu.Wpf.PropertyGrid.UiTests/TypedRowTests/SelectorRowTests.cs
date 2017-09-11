9ae8f5ce1c16be74fb686834eeaef0d925c8fccf
JohanLarsson <johan_gavle1@hotmail.com>
2017-09-11 15:36:11 +0200

    Refactor SelectorRowTests

--- Gu.Wpf.PropertyGrid.UiTests/TypedRowTests/SelectorRowTests.cs
+++ Gu.Wpf.PropertyGrid.UiTests/TypedRowTests/SelectorRowTests.cs
@@ -1,83 +1,83 @@
 namespace Gu.Wpf.PropertyGrid.UiTests
 {
     using System.Linq;
-
+    using Gu.Wpf.UiAutomation;
     using NUnit.Framework;
-    using TestStack.White.UIItems;
-    using TestStack.White.UIItems.ListBoxItems;
 
-    public class SelectorRowTests : WindowTests
+    public class SelectorRowTests
     {
-        private Button loseFocusButton;
-        private Label currentCultureTextBlock;
+        private static readonly string WindowName = "SelectorRowWindow";
 
-        private ComboBox defaultBox;
-        private ComboBox lostFocusBox;
-        private ComboBox readonlyBox;
-        private ComboBox editableBox;
 
-        protected override string WindowName { get; } = "SelectorRowWindow";
-
-        [OneTimeSetUp]
-        public override void OneTimeSetUp()
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
         {
-            base.OneTimeSetUp();
-            this.loseFocusButton = this.Window.GetByText<Button>("lose focus");
-            this.currentCultureTextBlock = this.Window.Get<Label>("currentCultureTextBlock");
-
-            this.defaultBox = this.Window.FindRow("default").Value<ComboBox>();
-            this.lostFocusBox = this.Window.FindRow("lostfocus").Value<ComboBox>();
-            this.readonlyBox = this.Window.FindRow("readonly").Value<ComboBox>();
-            this.editableBox = this.Window.FindRow("editable").Value<ComboBox>();
+            Application.KillLaunched(Info.ExeFileName);
         }
 
         [Test]
         public void UpdatesWhenLostFocus()
         {
-            this.defaultBox.Select("sv-SE");
-            Assert.AreEqual("sv-SE", this.lostFocusBox.SelectedItemText);
+            using (var app = Application.AttachOrLaunch(Info.ExeFileName, WindowName))
+            {
+                var window = app.MainWindow;
+                window.FindComboBoxRow("default").Value().Select("sv-SE");
+                Assert.AreEqual("sv-SE", window.FindComboBoxRow("lostfocus").Value().SelectedItem.Text);
 
-            this.lostFocusBox.EditableText = "en-US";
-            Assert.AreEqual("sv-SE", this.currentCultureTextBlock.Text);
+                window.FindComboBoxRow("lostfocus").Value().EditableText = "en-US";
+                Assert.AreEqual("sv-SE", window.FindTextBlock("currentCultureTextBlock").Text);
 
-            this.loseFocusButton.Click();
-            Assert.AreEqual("en-US", this.currentCultureTextBlock.Text);
+                window.FindButton("lose focus").Click();
+                Assert.AreEqual("en-US", window.FindTextBlock("currentCultureTextBlock").Text);
+            }
         }
 
         [Test]
         public void UpdatesWhenPropertyChanged()
         {
-            this.defaultBox.Select("en-US");
-            Assert.AreEqual("en-US", this.currentCultureTextBlock.Text);
-            Assert.AreEqual("en-US", this.defaultBox.SelectedItemText);
-            Assert.AreEqual("en-US", this.lostFocusBox.SelectedItemText);
-            Assert.AreEqual("en-US", this.readonlyBox.SelectedItemText);
-            Assert.AreEqual("en-US", this.editableBox.SelectedItemText);
+            using (var app = Application.AttachOrLaunch(Info.ExeFileName, WindowName))
+            {
+                var window = app.MainWindow;
+                window.FindComboBoxRow("default").Value().Select("en-US");
+                Assert.AreEqual("en-US", window.FindTextBlock("currentCultureTextBlock").Text);
+                Assert.AreEqual("en-US", window.FindComboBoxRow("default").Value().SelectedItem.Text);
+                Assert.AreEqual("en-US", window.FindComboBoxRow("lostfocus").Value().SelectedItem.Text);
+                Assert.AreEqual("en-US", window.FindComboBoxRow("readonly").Value().SelectedItem.Text);
+                Assert.AreEqual("en-US", window.FindComboBoxRow("editable").Value().SelectedItem.Text);
 
-            this.defaultBox.Select("sv-SE");
-            Assert.AreEqual("sv-SE", this.currentCultureTextBlock.Text);
-            Assert.AreEqual("sv-SE", this.defaultBox.SelectedItemText);
-            Assert.AreEqual("sv-SE", this.lostFocusBox.SelectedItemText);
-            Assert.AreEqual("sv-SE", this.readonlyBox.SelectedItemText);
-            Assert.AreEqual("sv-SE", this.editableBox.SelectedItemText);
+                window.FindComboBoxRow("default").Value().Select("sv-SE");
+                Assert.AreEqual("sv-SE", window.FindTextBlock("currentCultureTextBlock").Text);
+                Assert.AreEqual("sv-SE", window.FindComboBoxRow("default").Value().SelectedItem.Text);
+                Assert.AreEqual("sv-SE", window.FindComboBoxRow("lostfocus").Value().SelectedItem.Text);
+                Assert.AreEqual("sv-SE", window.FindComboBoxRow("readonly").Value().SelectedItem.Text);
+                Assert.AreEqual("sv-SE", window.FindComboBoxRow("editable").Value().SelectedItem.Text);
+            }
         }
 
         [Test]
         public void IsEditable()
         {
-            Assert.False(this.defaultBox.IsEditable);
-            Assert.True(this.lostFocusBox.IsEditable);
-            Assert.False(this.readonlyBox.IsEditable);
-            Assert.True(this.editableBox.IsEditable);
+            using (var app = Application.AttachOrLaunch(Info.ExeFileName, WindowName))
+            {
+                var window = app.MainWindow;
+                Assert.False(window.FindComboBoxRow("default").Value().IsEditable);
+                Assert.True(window.FindComboBoxRow("lostfocus").Value().IsEditable);
+                Assert.False(window.FindComboBoxRow("readonly").Value().IsEditable);
+                Assert.True(window.FindComboBoxRow("editable").Value().IsEditable);
+            }
         }
 
         [Test]
         public void Items()
         {
-            var expected = new[] { "sv-SE", "en-US" };
-            CollectionAssert.AreEqual(expected, this.defaultBox.Items.AsReadOnly().Select(x => x.Text));
-            CollectionAssert.AreEqual(expected, this.lostFocusBox.Items.AsReadOnly().Select(x => x.Text));
-            CollectionAssert.AreEqual(expected, this.readonlyBox.Items.AsReadOnly().Select(x => x.Text));
+            using (var app = Application.AttachOrLaunch(Info.ExeFileName, WindowName))
+            {
+                var window = app.MainWindow;
+                var expected = new[] { "sv-SE", "en-US" };
+                CollectionAssert.AreEqual(expected, window.FindComboBoxRow("default").Value().Items.Select(x => x.Text));
+                CollectionAssert.AreEqual(expected, window.FindComboBoxRow("lostfocus").Value().Items.Select(x => x.Text));
+                CollectionAssert.AreEqual(expected, window.FindComboBoxRow("readonly").Value().Items.Select(x => x.Text));
+            }
         }
     }
 }
