6a4ba10b8110db43bfef8ac69f15bf36beed7f23
JohanLarsson <johan_gavle1@hotmail.com>
2017-09-11 15:39:52 +0200

    Refactor StringRowTests

--- Gu.Wpf.PropertyGrid.UiTests/TypedRowTests/StringRowTests.cs
+++ Gu.Wpf.PropertyGrid.UiTests/TypedRowTests/StringRowTests.cs
@@ -1,67 +1,70 @@
 namespace Gu.Wpf.PropertyGrid.UiTests
 {
+    using Gu.Wpf.UiAutomation;
     using NUnit.Framework;
 
-    using TestStack.White.UIItems;
-
-    public class StringRowTests : WindowTests
+    public class StringRowTests
     {
-        private Button loseFocusButton;
-        private TextBox currentValueTextBox;
-        private TextBox defaultBox;
-        private TextBox propertychangedBox;
-        private TextBox readonlyBox;
+        private static readonly string WindowName = "StringRowWindow";
 
-        protected override string WindowName { get; } = "StringRowWindow";
-
-        [OneTimeSetUp]
-        public override void OneTimeSetUp()
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
         {
-            base.OneTimeSetUp();
-            this.loseFocusButton = this.Window.GetByText<Button>("lose focus");
-            this.currentValueTextBox = this.Window.Get<TextBox>("currentValueTextBox");
-
-            this.defaultBox = this.Window.FindRow("default").Value<TextBox>();
-            this.propertychangedBox = this.Window.FindRow("propertychanged").Value<TextBox>();
-            this.readonlyBox = this.Window.FindRow("readonly string").Value<TextBox>();
+            Application.KillLaunched(Info.ExeFileName);
         }
 
         [SetUp]
         public void SetUp()
         {
-            this.currentValueTextBox.Text = string.Empty;
-            this.loseFocusButton.Click();
+            using (var app = Application.AttachOrLaunch(Info.ExeFileName, WindowName))
+            {
+                var window = app.MainWindow;
+                window.FindTextBox("currentValueTextBox").Text = string.Empty;
+                window.FindButton("lose focus").Click();
+            }
         }
 
         [Test]
         public void UpdatesWhenLostFocus()
         {
-            this.defaultBox.Text = "1";
-            Assert.AreEqual("1", this.defaultBox.Text);
-            Assert.AreEqual(string.Empty, this.propertychangedBox.Text);
-            Assert.AreEqual(string.Empty, this.readonlyBox.Text);
+            using (var app = Application.AttachOrLaunch(Info.ExeFileName, WindowName))
+            {
+                var window = app.MainWindow;
+                window.FindTextBoxRow("default").Value().Text = "1";
+                Assert.AreEqual("1", window.FindTextBoxRow("default").Value().Text);
+                Assert.AreEqual(string.Empty, window.FindTextBoxRow("propertychanged").Value().Text);
+                Assert.AreEqual(string.Empty, window.FindTextBoxRow("readonly string").Value().Text);
 
-            this.loseFocusButton.Click();
-            Assert.AreEqual("1", this.defaultBox.Text);
-            Assert.AreEqual("1", this.propertychangedBox.Text);
-            Assert.AreEqual("1", this.readonlyBox.Text);
+                window.FindButton("lose focus").Click();
+                Assert.AreEqual("1", window.FindTextBoxRow("default").Value().Text);
+                Assert.AreEqual("1", window.FindTextBoxRow("propertychanged").Value().Text);
+                Assert.AreEqual("1", window.FindTextBoxRow("readonly string").Value().Text);
+            }
         }
 
         [Test]
         public void UpdatesWhenPropertyChanged()
         {
-            this.propertychangedBox.Text = "2";
-            Assert.AreEqual("2", this.defaultBox.Text);
-            Assert.AreEqual("2", this.propertychangedBox.Text);
-            Assert.AreEqual("2", this.readonlyBox.Text);
+            using (var app = Application.AttachOrLaunch(Info.ExeFileName, WindowName))
+            {
+                var window = app.MainWindow;
+                window.FindTextBoxRow("propertychanged").Value().Text = "2";
+                Assert.AreEqual("2", window.FindTextBoxRow("default").Value().Text);
+                Assert.AreEqual("2", window.FindTextBoxRow("propertychanged").Value().Text);
+                Assert.AreEqual("2", window.FindTextBoxRow("readonly string").Value().Text);
+            }
         }
 
         [Test]
         public void IsReadonly()
         {
-            Assert.IsFalse(this.defaultBox.IsReadOnly);
-            Assert.IsFalse(this.propertychangedBox.IsReadOnly);
-            Assert.IsTrue(this.readonlyBox.IsReadOnly);
+            using (var app = Application.AttachOrLaunch(Info.ExeFileName, WindowName))
+            {
+                var window = app.MainWindow;
+                Assert.IsFalse(window.FindTextBoxRow("default").Value().IsReadOnly);
+                Assert.IsFalse(window.FindTextBoxRow("propertychanged").Value().IsReadOnly);
+                Assert.IsTrue(window.FindTextBoxRow("readonly string").Value().IsReadOnly);
+            }
         }
     }
 }
