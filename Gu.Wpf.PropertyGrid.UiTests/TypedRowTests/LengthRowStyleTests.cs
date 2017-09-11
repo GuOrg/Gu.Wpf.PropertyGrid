91bb1a3a676b7f3b3a70777e571e9200f4ec681c
JohanLarsson <johan_gavle1@hotmail.com>
2017-09-11 13:53:10 +0200

    Refactor LengthRowStyleTests

--- Gu.Wpf.PropertyGrid.UiTests/TypedRowTests/LengthRowStyleTests.cs
+++ Gu.Wpf.PropertyGrid.UiTests/TypedRowTests/LengthRowStyleTests.cs
@@ -1,22 +1,26 @@
 namespace Gu.Wpf.PropertyGrid.UiTests
 {
+    using Gu.Wpf.UiAutomation;
     using NUnit.Framework;
-    using TestStack.White.UIItems;
 
-    public class LengthRowStyleTests : WindowTests
+    public class LengthRowStyleTests
     {
-        protected override string WindowName { get; } = "LengthRowStyleWindow";
+        private static readonly string WindowName = "LengthRowStyleWindow";
 
         [Test]
         public void CorrectValuesAndSuffixes()
         {
-            var defaultRow = this.Window.FindRow("default");
-            Assert.AreEqual("\u00A0mm", defaultRow.Suffix().Text);
-            Assert.AreEqual("12.3456", defaultRow.Value<TextBox>().Text);
+            using (var app = Application.AttachOrLaunch(Info.ExeFileName, WindowName))
+            {
+                var window = app.MainWindow;
+                var defaultRow = window.FindTextBoxRow("default");
+                Assert.AreEqual("\u00A0mm", defaultRow.Suffix().Text);
+                Assert.AreEqual("12.3456", defaultRow.Value().Text);
 
-            var cmRow = this.Window.FindRow("explicit cm");
-            Assert.AreEqual("\u00A0cm", cmRow.Suffix().Text);
-            Assert.AreEqual("1.23456", cmRow.Value<TextBox>().Text);
+                var cmRow = window.FindTextBoxRow("explicit cm");
+                Assert.AreEqual("\u00A0cm", cmRow.Suffix().Text);
+                Assert.AreEqual("1.23456", cmRow.Value().Text);
+            }
         }
     }
 }
