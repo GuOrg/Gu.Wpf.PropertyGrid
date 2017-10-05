34fe0dc2d09947ac7bcf7afa6670230cdcac6b66
JohanLarsson <johan_gavle1@hotmail.com>
2017-10-05 17:40:38 +0200

    Sign, fix #40.

--- Gu.Wpf.PropertyGrid/Properties/AssemblyInfo.cs
+++ Gu.Wpf.PropertyGrid/Properties/AssemblyInfo.cs
@@ -4,9 +4,6 @@ using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Markup;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Gu.Wpf.PropertyGrid")]
 [assembly: AssemblyDescription("Small project for creating property grids in wpf.")]
 [assembly: AssemblyConfiguration("")]
@@ -15,39 +12,15 @@ using System.Windows.Markup;
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// In order to begin building localizable applications, set
-// <UICulture>CultureYouAreCodingWith</UICulture> in your .csproj file
-// inside a <PropertyGroup>.  For example, if you are using US english
-// in your source files, set the <UICulture> to en-US.  Then uncomment
-// the NeutralResourceLanguage attribute below.  Update the "en-US" in
-// the line below to match the UICulture setting in the project file.
-
-//// [assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 [assembly:ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.0.0.0")]
 [assembly: AssemblyFileVersion("3.0.0.0")]
 
-[assembly: InternalsVisibleTo("Gu.Wpf.PropertyGrid.Tests", AllInternalsVisible = true)]
-[assembly: InternalsVisibleTo("Gu.Wpf.PropertyGrid.NumericRows", AllInternalsVisible = true)]
-[assembly: InternalsVisibleTo("Gu.Wpf.PropertyGrid.UnitRows", AllInternalsVisible = true)]
-[assembly: InternalsVisibleTo("Gu.Wpf.PropertyGrid.UnitRows.Tests", AllInternalsVisible = true)]
+[assembly: InternalsVisibleTo("Gu.Wpf.PropertyGrid.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100037C4C4170A5437C55B453ECF5D663968EFB1C2D485AE86FA696A39AB98C7401A0915080E9B4A646632D283B65B2D0295DFCDE976B35B5057F5AB7C45654FDBC00DF4836E973E77B7595E07C4BA73A7BBE8A98DB5DB3F0C6A75508A856F2E2A9E354269FC67A42D8156ACEE12C44FFA48361DEB8A363FFBF69948948520438C1", AllInternalsVisible = true)]
+[assembly: InternalsVisibleTo("Gu.Wpf.PropertyGrid.NumericRows, PublicKey=0024000004800000940000000602000000240000525341310004000001000100037C4C4170A5437C55B453ECF5D663968EFB1C2D485AE86FA696A39AB98C7401A0915080E9B4A646632D283B65B2D0295DFCDE976B35B5057F5AB7C45654FDBC00DF4836E973E77B7595E07C4BA73A7BBE8A98DB5DB3F0C6A75508A856F2E2A9E354269FC67A42D8156ACEE12C44FFA48361DEB8A363FFBF69948948520438C1", AllInternalsVisible = true)]
+[assembly: InternalsVisibleTo("Gu.Wpf.PropertyGrid.UnitRows, PublicKey=0024000004800000940000000602000000240000525341310004000001000100037C4C4170A5437C55B453ECF5D663968EFB1C2D485AE86FA696A39AB98C7401A0915080E9B4A646632D283B65B2D0295DFCDE976B35B5057F5AB7C45654FDBC00DF4836E973E77B7595E07C4BA73A7BBE8A98DB5DB3F0C6A75508A856F2E2A9E354269FC67A42D8156ACEE12C44FFA48361DEB8A363FFBF69948948520438C1", AllInternalsVisible = true)]
+[assembly: InternalsVisibleTo("Gu.Wpf.PropertyGrid.UnitRows.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100037C4C4170A5437C55B453ECF5D663968EFB1C2D485AE86FA696A39AB98C7401A0915080E9B4A646632D283B65B2D0295DFCDE976B35B5057F5AB7C45654FDBC00DF4836E973E77B7595E07C4BA73A7BBE8A98DB5DB3F0C6A75508A856F2E2A9E354269FC67A42D8156ACEE12C44FFA48361DEB8A363FFBF69948948520438C1", AllInternalsVisible = true)]
 
 [assembly: XmlnsDefinition("http://gu.se/PropertyGrid", "Gu.Wpf.PropertyGrid")]
 [assembly: XmlnsPrefix("http://gu.se/PropertyGrid", "propertyGrid")]
