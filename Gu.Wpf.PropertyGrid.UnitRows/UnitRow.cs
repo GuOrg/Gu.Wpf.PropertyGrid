0e93b0e97a88f8c6178c5bde9982a9f9151ecb7f
JohanLarsson <johan_gavle1@hotmail.com>
2017-10-06 09:42:17 +0200

    Added some generic docs, just noise to mute StyleCop.

--- Gu.Wpf.PropertyGrid.UnitRows/UnitRow.cs
+++ Gu.Wpf.PropertyGrid.UnitRows/UnitRow.cs
@@ -4,38 +4,51 @@
     using Gu.Units;
     using Gu.Wpf.NumericInput;
 
+    /// <summary>
+    /// Base class for unit row.
+    /// </summary>
     public abstract class UnitRow : Row
     {
+        /// <summary>Identifies the <see cref="ScalarValue"/> dependency property.</summary>
         public static readonly DependencyProperty ScalarValueProperty = DependencyProperty.Register(
-            "ScalarValue",
+            nameof(ScalarValue),
             typeof(double?),
             typeof(UnitRow),
             new PropertyMetadata(default(double?), OnScalarValueChanged));
 
+        /// <summary>Identifies the <see cref="ScalarMinValue"/> dependency property.</summary>
         public static readonly DependencyProperty ScalarMinValueProperty = DependencyProperty.Register(
-            "ScalarMinValue",
+            nameof(ScalarMinValue),
             typeof(double?),
             typeof(UnitRow),
             new PropertyMetadata(default(double?), OnScalarMinValueChanged));
 
+        /// <summary>Identifies the <see cref="ScalarMaxValue"/> dependency property.</summary>
         public static readonly DependencyProperty ScalarMaxValueProperty = DependencyProperty.Register(
-            "ScalarMaxValue",
+            nameof(ScalarMaxValue),
             typeof(double?),
             typeof(UnitRow),
             new PropertyMetadata(default(double?), OnScalarMaxValueChanged));
 
+        /// <summary> The default symbol format. </summary>
         public static readonly SymbolFormat DefaultSymbolFormat = SymbolFormat.Default;
 
+        /// <summary>Identifies the <see cref="SymbolFormat"/> dependency property.</summary>
         public static readonly DependencyProperty SymbolFormatProperty = DependencyProperty.RegisterAttached(
-            "SymbolFormat",
+            nameof(SymbolFormat),
             typeof(SymbolFormat),
             typeof(UnitRow),
-            new FrameworkPropertyMetadata(DefaultSymbolFormat, FrameworkPropertyMetadataOptions.Inherits));
+            new FrameworkPropertyMetadata(
+                DefaultSymbolFormat,
+                FrameworkPropertyMetadataOptions.Inherits,
+                OnSymbolFormatChanged));
 
+        /// <summary>Identifies the <see cref="DecimalDigits"/> dependency property.</summary>
         public static readonly DependencyProperty DecimalDigitsProperty = NumericBox.DecimalDigitsProperty.AddOwner(
             typeof(UnitRow),
             new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.Inherits));
 
+        /// <summary>Identifies the <see cref="CanValueBeNull"/> dependency property.</summary>
         public static readonly DependencyProperty CanValueBeNullProperty = NumericBox.CanValueBeNullProperty.AddOwner(
             typeof(UnitRow),
             new FrameworkPropertyMetadata(false, FrameworkPropertyMetadataOptions.Inherits));
@@ -45,68 +58,124 @@
             DefaultStyleKeyProperty.OverrideMetadata(typeof(UnitRow), new FrameworkPropertyMetadata(typeof(UnitRow)));
         }
 
+        /// <summary>
+        /// Gets or sets the Value property as a double.
+        /// </summary>
         public double? ScalarValue
         {
             get => (double?)this.GetValue(ScalarValueProperty);
             set => this.SetValue(ScalarValueProperty, value);
         }
 
+        /// <summary>
+        /// Gets or sets the MinValue property as a double.
+        /// </summary>
         public double? ScalarMinValue
         {
             get => (double?)this.GetValue(ScalarMinValueProperty);
             set => this.SetValue(ScalarMinValueProperty, value);
         }
 
+        /// <summary>
+        /// Gets or sets the MaxValue property as a double.
+        /// </summary>
         public double? ScalarMaxValue
         {
             get => (double?)this.GetValue(ScalarMaxValueProperty);
             set => this.SetValue(ScalarMaxValueProperty, value);
         }
 
+        /// <summary>
+        /// Gets or sets the <see cref="SymbolFormat"/> to use when formatting the suffix.
+        /// </summary>
         public SymbolFormat SymbolFormat
         {
             get => (SymbolFormat)this.GetValue(SymbolFormatProperty);
             set => this.SetValue(SymbolFormatProperty, value);
         }
 
+        /// <summary>
+        /// Gets or sets the number of decimal digits:
+        /// DecimalDigits="3" sets StringFormat to F3
+        /// DecimalDigits="-3" sets StringFormat to 0.###
+        /// Default is null.
+        /// </summary>
         public int? DecimalDigits
         {
             get => (int?)this.GetValue(DecimalDigitsProperty);
             set => this.SetValue(DecimalDigitsProperty, value);
         }
 
+        /// <summary>
+        /// Gets or sets a value indicating whether null is an allowed value for the Value property
+        /// </summary>
         public bool CanValueBeNull
         {
             get => (bool)this.GetValue(CanValueBeNullProperty);
             set => this.SetValue(CanValueBeNullProperty, value);
         }
 
-        public static void SetSymbolFormat(UIElement element, SymbolFormat value)
-        {
-            element.SetValue(SymbolFormatProperty, value);
-        }
-
+        /// <summary>
+        /// DependencyProperty getter for <see cref="SymbolFormat" /> property.
+        /// </summary>
+        /// <param name="element">The element from which to read the attached property.</param>
+        /// <returns>The current value of the property.</returns>
         public static SymbolFormat GetSymbolFormat(UIElement element)
         {
             return (SymbolFormat)element.GetValue(SymbolFormatProperty);
         }
 
-        protected static void OnScalarValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        /// <summary>
+        /// DependencyProperty setter for <see cref="SymbolFormat" /> property.
+        /// </summary>
+        /// <param name="element">The element to which to write the attached property.</param>
+        /// <param name="value">The property value to set</param>
+        public static void SetSymbolFormat(UIElement element, SymbolFormat value)
+        {
+            element.SetValue(SymbolFormatProperty, value);
+        }
+
+        /// <summary>
+        /// Create a suffix sting for the current Unit
+        /// </summary>
+        /// <param name="symbolFormat">The symbol format to use.</param>
+        /// <returns>The string representation of the unit including leading whitespace if any.</returns>
+        protected abstract string CreateSuffix(SymbolFormat symbolFormat);
+
+        /// <summary>
+        /// Called when the <see cref="ScalarValue"/> changes.
+        /// </summary>
+        /// <param name="newValue">The new value.</param>
+        protected abstract void OnScalarValueChanged(double? newValue);
+
+        /// <summary>
+        /// Called when the <see cref="ScalarMinValue"/> changes.
+        /// </summary>
+        /// <param name="newValue">The new value.</param>
+        protected abstract void OnScalarMinValueChanged(double? newValue);
+
+        /// <summary>
+        /// Called when the <see cref="ScalarMaxValue"/> changes.
+        /// </summary>
+        /// <param name="newValue">The new value.</param>
+        protected abstract void OnScalarMaxValueChanged(double? newValue);
+
+        private static void OnScalarValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             ((UnitRow)d).OnScalarValueChanged((double?)e.NewValue);
         }
 
-        protected static void OnScalarMinValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        private static void OnScalarMinValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             ((UnitRow)d).OnScalarMinValueChanged((double?)e.NewValue);
         }
 
-        protected static void OnScalarMaxValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        private static void OnScalarMaxValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             ((UnitRow)d).OnScalarMaxValueChanged((double?)e.NewValue);
         }
 
-        protected static void OnSymbolFormatChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        private static void OnSymbolFormatChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             var row = (UnitRow)d;
             var oldSuffix = row.CreateSuffix((SymbolFormat)e.OldValue);
@@ -118,13 +187,5 @@
                 row.SetCurrentValue(SuffixProperty, row.CreateSuffix((SymbolFormat)e.NewValue));
             }
         }
-
-        protected abstract string CreateSuffix(SymbolFormat symbolFormat);
-
-        protected abstract void OnScalarValueChanged(double? newValue);
-
-        protected abstract void OnScalarMinValueChanged(double? newValue);
-
-        protected abstract void OnScalarMaxValueChanged(double? newValue);
     }
 }
