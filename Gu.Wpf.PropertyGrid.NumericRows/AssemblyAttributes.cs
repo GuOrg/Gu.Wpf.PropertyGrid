31ae777cad857f52aaa3547bf8b6d3efd1f8f6a3
JohanLarsson <johan_gavle1@hotmail.com>
2020-10-29 12:02:07 +0100

    Fix tests.

--- Gu.Wpf.PropertyGrid.NumericRows/AssemblyAttributes.cs
+++ Gu.Wpf.PropertyGrid.NumericRows/AssemblyAttributes.cs
@@ -1,9 +1,6 @@
-using System.Reflection;
 using System.Windows;
 using System.Windows.Markup;
 
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 [assembly: XmlnsDefinition("http://gu.se/PropertyGrid", "Gu.Wpf.PropertyGrid.NumericRows", AssemblyName = "Gu.Wpf.PropertyGrid.NumericRows")]
 [assembly: XmlnsPrefix("http://gu.se/PropertyGrid", "propertyGrid")]
--- Gu.Wpf.PropertyGrid.UnitRows/AssemblyAttributes.cs
+++ Gu.Wpf.PropertyGrid.UnitRows/AssemblyAttributes.cs
@@ -2,6 +2,6 @@ using System.Windows;
 using System.Windows.Markup;
 
 //// [assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-[assembly:ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 [assembly: XmlnsDefinition("http://gu.se/PropertyGrid", "Gu.Wpf.PropertyGrid.UnitRows")]
 [assembly: XmlnsPrefix("http://gu.se/PropertyGrid", "propertyGrid")]
