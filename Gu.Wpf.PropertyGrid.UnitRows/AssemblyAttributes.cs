0b7106b34a9b416fb7c2086b40d83f76d30d47b4
JohanLarsson <johan_gavle1@hotmail.com>
2020-10-29 11:13:54 +0100

    Use new csproj and nuget

---
+++ Gu.Wpf.PropertyGrid.UnitRows/AssemblyAttributes.cs
@@ -0,0 +1,7 @@
+using System.Windows;
+using System.Windows.Markup;
+
+//// [assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+[assembly:ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: XmlnsDefinition("http://gu.se/PropertyGrid", "Gu.Wpf.PropertyGrid.UnitRows")]
+[assembly: XmlnsPrefix("http://gu.se/PropertyGrid", "propertyGrid")]
